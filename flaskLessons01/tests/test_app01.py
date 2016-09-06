282bf62ca79d590e152556db5440d51de0db19eb
Ricardo Portela da Silva <ricaportela@gmail.com>
2016-09-05 23:39:10 -0300

    Update files tests

--- flaskLessons01/tests/test_app01.py
+++ flaskLessons01/tests/test_app01.py
@@ -6,6 +6,10 @@ from flaskLessons01.learning01 import app
 @pytest.mark.usefixtures('client_class')
 class TestApp:
     @pytest.fixture
+    def app(self):
+        
+        return app
+
     def test_home(self):
         response = self.client.get("/")
         assert response.data.decode('utf-8') == "<h1>Testando...</h1>"
